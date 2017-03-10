4280774738c5ae36f915e4067337a4f479106f9b
Rails Udemy Course <user@example.com>
2017-03-10 19:37:34 +0000

    add user and stock association and display

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,31 +1,36 @@
-var init_stock_lookup;
+
+
+init_stock_lookup;
 
 init_stock_lookup = function() {
     $('#stock-lookup-form').on('ajax:before', function(event, data, status){
-        show_spinner()
+        show_spinner();
     });
     
       $('#stock-lookup-form').on('ajax:after', function(event, data, status){
-        hide_spinner()
+        hide_spinner();
     });
     
     $('#stock-lookup-form').on('ajax:success', function(event, data, status){
+      alert("hello");
       $('#stock-lookup').replaceWith(data);
         init_stock_lookup();
     });
     
+//}
+
+    $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+        console.log("test");
+        hide_spinner();
+        $('#stock-lookup-results').replaceWith(' ');
+        $('#stock-lookup-errors').replaceWith('Stock was not found');
+    });
+
 }
-    
-//     $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
-//         hide_spinner();
-//         $('#stock-lookup-results').replaceWith(' ');
-//         $('#stock-lookup-errors').replaceWith('Stock was not found');
-//     })
-// }
 
 
 $(document).ready(function() {
-   init_stock_lookup();
+  init_stock_lookup();
     
 })
 
